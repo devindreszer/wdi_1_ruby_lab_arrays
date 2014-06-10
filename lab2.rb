f5f88226ee85fca57530fda8f6e533ec73e8389b
Devin Dreszer <devindreszer@gmail.com>
2014-06-10 12:28:08 -0400

    Complete Lab 2

---
+++ lab2.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+tweet = "We ate fifty cheeseburgers for lunch"
+
+tweet_split = tweet.split(" ")
+
+tweets = tweet_split.select do |word|
+  word.length > 3
+end
+
+hashtagged = tweets.map do |tweet|
+  "##{tweet}"
+end
