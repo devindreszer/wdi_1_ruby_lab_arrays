1c5e8a5a36355719015e72de0e0a4e9a491a67b9
Devin Dreszer <devindreszer@gmail.com>
2014-06-10 12:49:51 -0400

    Complete challenge to add hashtags to original tweet

--- lab2.rb
+++ lab2.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 tweet = "We ate fifty cheeseburgers for lunch"
 
-tweet_split = tweet.split(" ")
+tweet_split = tweet.split
 
 tweets = tweet_split.select do |word|
   word.length > 3
@@ -11,3 +11,9 @@ end
 hashtagged = tweets.map do |tweet|
   "##{tweet}"
 end
+
+hashtagged.each do |hashtags|
+  tweet << " #{hashtags}"
+end
+
+binding.pry
