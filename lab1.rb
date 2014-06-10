b641c93973952f8b4eb1e4cf38afddf00d025c25
Devin Dreszer <devindreszer@gmail.com>
2014-06-10 11:56:27 -0400

    Complete Lab 1

---
+++ lab1.rb
@@ -0,0 +1,25 @@
+# 1
+days_of_week = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+
+# 2
+days_of_week.unshift(days_of_week.pop)
+
+# 3
+days_of_week[days_of_week.index('Thursday')].upcase!
+
+# 4
+weekdays = %w(Monday Tuesday Wednesday Thursday Friday)
+weekends = %(Saturday Sunday)
+days_of_week_split = [weekdays, weekends]
+
+# 5
+days_of_week_split[0][2] = "Woden's Day"
+
+# 6
+days_of_week_split.delete_at(0)
+
+# 7
+days_of_week.sort!
+
+# 8
+puts days_of_week
